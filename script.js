04813f88f3c664177f6e3627d88d4c3a6e4d0767
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-01-20 23:37:56 -0600

    my changes

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+
 $( document ).ready(function() {
     console.log( "ready!" );
-    $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?q=United-States,uk&appid=b6907d289e10d714a6e88b30761fae22", success: function(result){
-    console.log(result);
+    
+    $.ajax({url: https:<href>samples.openweathermap.org/data/2.5/forecast?id=524901&lang=zh_cn&appid=dbba5cf9769115cf358f678b78a6c98c696a7cf1</href>
       }});
 });
+// Select Elements:
+const dateElement = document.querySelector(".date");
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const humidityElement = document.querySelector(".humidity-value p")
+
+// Weather Data:
+
+
+
